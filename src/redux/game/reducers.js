2a8ec36252c35c1af3b04aeef9d235e02228a186
Richard Jewson <richard.jewson@doclerholding.com>
2017-08-28 17:12:00 +0200

    remove extra

--- src/redux/game/reducers.js
+++ src/redux/game/reducers.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "./actionTypes";
-import * as grid from "../../util/grid";
+//import * as grid from "../../util/grid";
 
 const initialState = {
   grid: []
@@ -9,7 +9,7 @@ export default function favorites(state = initialState, action) {
   switch (action.type) {
     case actionTypes.RESET_GAME:
       const { width, height, mines } = action.payload;
-      const emptyGrid = grid.initGrid(width, height, 0);
+      const emptyGrid = [];//grid.initGrid(width, height, 0);
       return Object.assign({}, initialState, {
         grid: emptyGrid
       });
